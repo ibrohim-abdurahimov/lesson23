f3678ddd104101ea3c3d0945ba39d691a0150a33
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-08-24 17:25:44 +0500

    masalalar

---
+++ app.js
@@ -0,0 +1,84 @@
+// masala 1
+{
+    // let a = prompt("Enter side A of the square")
+    // let p =4*a
+    // console.log(`Perimeter of the square: ${p}`)
+}
+
+// masala 2
+{
+    // let a = prompt("Enter side A of the square")
+    // let s =Math.pow(a,2)
+    // console.log(`Surface area of ​​a square: ${s}`)
+}
+
+// masala 3
+{
+    // let a = +prompt("Enter side A of the square")
+    // let b = +prompt("Enter side B of the square")
+    // let s = a*b
+    // let p = 2*(a+b)
+    // console.log(`Perimeter of the square: ${p}`)
+    // console.log(`Surface area of ​​a square: ${s}`)
+}
+
+// masal 4
+{
+    // let d = prompt("Enter the diameter of the shaft")
+    // let p =Math.PI*d
+    // console.log(`The diameter of the shaft: ${p}`)
+}
+
+// masala 5
+{
+    // let a = prompt("Enter side A of the cube")
+    // let v =Math.pow(a,3)
+    // let s =6*Math.pow(a,2)
+    // console.log(`The volume of the cube: ${v}`)
+    // console.log(`The full surface of a cube: ${s}`)
+}
+
+// masala 6
+{
+    // let a = +prompt("Enter side A of the parallelepiped")
+    // let b = +prompt("Enter side B of the parallelepiped")
+    // let c = +prompt("Enter side C of the parallelepiped")
+    // let v = a*b*c
+    // let s = 2*(a*b+b*c+a*c)
+    // console.log(`The volume of the cube: ${v}`)
+    // console.log(`The full surface of a cube: ${s}`)
+
+}
+
+// masala 7
+{
+    // let r = prompt("Enter the radius of the circle")
+    // let l = 2*Math.PI*r
+    // let s = Math.PI*Math.pow(r,2)
+    // console.log(`The length of the circle: ${l}`)
+    // console.log(`The full surface of the circle: ${s}`)
+}
+
+// masala 8
+{
+    // let a = +prompt("Enter the number A")
+    // let b = +prompt("Enter the number B")
+    // console.log(`Results: ${(a+b)/2}`)
+}
+
+// masala 9
+{
+    // let a = prompt("Enter a positive number A")
+    // let b = prompt("Enter a positive number B")
+    // console.log(`Result: ${Math.sqrt(a*b)}`)
+}
+
+// masala 10
+{
+    // let num1 = +prompt("Enter the first number")
+    // let num2 = +prompt("Enter the second number")
+    // console.log(`The sum of the numbers: ${num1+num2}`)
+    // console.log(`Multiplication of numbers: ${num1*num2}`)
+    // console.log(`The square of the first number: ${Math.pow(num1,2)}`)
+    // console.log(`The square of the second number: ${Math.pow(num2,2)}`)
+}
